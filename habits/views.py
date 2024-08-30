c34fb0ea0eeec90b245a8ae3beceb08b0db9ad26
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-31 02:58:35 +0400

    Add IsOwner permission to HabitViewSet and update list() method in it, also update get_queryset() method in HabitPublicListAPIView

--- habits/views.py
+++ habits/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, generics
 from habits.models import Habit
+from habits.permissions import IsOwner
 from habits.serializers import HabitSerializer
 
 
@@ -7,7 +8,19 @@ class HabitViewSet(viewsets.ModelViewSet):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
 
+    def get_permissions(self):
+        if self.action != 'list':
+            self.permission_classes = [IsOwner]
+
+    def list(self, request, *args, **kwargs):
+        self.queryset = Habit.objects.filter(user=request.user)
+        return super().list(request, *args, **kwargs)
+
 
 class HabitPublicListAPIView(generics.ListAPIView):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
+
+    def get_queryset(self):
+        self.queryset = Habit.objects.exclude(user=self.request.user).filter(is_public=True)
+        return super().get_queryset()
