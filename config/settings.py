85f93eaa1ae2b9f8af70d2f6a07f9beec5711743
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-31 02:57:23 +0400

    Add DEFAULT_PERMISSION_CLASSES setting to REST_FRAMEWORK dict

--- config/settings.py
+++ config/settings.py
@@ -131,9 +131,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
 }
