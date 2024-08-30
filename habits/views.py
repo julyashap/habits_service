de1659bc0b16c27123be90beda3bd786fdaf72d3
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-31 02:42:33 +0400

    Add HabitViewSet and HabitPublicListAPIView

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
+from habits.models import Habit
+from habits.serializers import HabitSerializer
 
-# Create your views here.
+
+class HabitViewSet(viewsets.ModelViewSet):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitPublicListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
