6b25825b1cb6e6cf7c70bda521670f461f89d694
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-31 15:18:12 +0400

    Add UserUpdateAPIView

--- users/views.py
+++ users/views.py
@@ -1,8 +1,15 @@
 from rest_framework import generics
 from users.models import User
+from users.permissions import IsCurrentUser
 from users.serializers import UserSerializer
 
 
 class UserCreateAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class UserUpdateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsCurrentUser]
