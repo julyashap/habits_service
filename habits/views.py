2300fa184d443c94e7b2d3e8018054fd54f02425
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-31 16:43:52 +0400

    Update HabitPublicListAPIView serializer with HabitPublicSerializer

--- habits/views.py
+++ habits/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, generics
 from habits.models import Habit
 from habits.paginators import HabitPaginator
 from habits.permissions import IsOwner
-from habits.serializers import HabitSerializer
+from habits.serializers import HabitSerializer, HabitPublicSerializer
 from habits.services import NOW, create_periodic_task
 
 
@@ -15,7 +15,7 @@ class HabitViewSet(viewsets.ModelViewSet):
         habit = serializer.save(user=self.request.user, created_at=NOW)
         habit.save()
 
-        if habit.user.tg_chat_id:
+        if not habit.is_enjoyable and habit.user.tg_chat_id:
             create_periodic_task(habit.periodicity, habit.pk)
 
     def get_permissions(self):
@@ -28,7 +28,7 @@ class HabitViewSet(viewsets.ModelViewSet):
 
 
 class HabitPublicListAPIView(generics.ListAPIView):
-    serializer_class = HabitSerializer
+    serializer_class = HabitPublicSerializer
     queryset = Habit.objects.all()
     pagination_class = HabitPaginator
 
