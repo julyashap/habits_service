aa33eb588b665c52cf903728f603e508b2273e40
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-31 17:39:39 +0400

    Update HabitOrRewardValidator and EnjoyableHabitValidator

--- habits/validators.py
+++ habits/validators.py
@@ -8,12 +8,13 @@ class HabitOrRewardValidator:
         related_habit = dict(value).get('related_habit')
         reward = dict(value).get('reward')
 
-        if not is_current_habit_enjoyable and related_habit and reward:
-            raise ValidationError('У полезной привычки нельзя указывать связанную '
-                                  'привычку и вознаграждение одновременно!')
-        elif not is_current_habit_enjoyable and not related_habit and not reward:
-            raise ValidationError('У полезной привычки обязательно должна быть либо связанная '
-                                  'привычка, либо вознаграждение!')
+        if not is_current_habit_enjoyable:
+            if related_habit and reward:
+                raise ValidationError('У полезной привычки нельзя указывать связанную '
+                                      'привычку и вознаграждение одновременно!')
+            elif not related_habit and not reward:
+                raise ValidationError('У полезной привычки обязательно должна быть либо связанная '
+                                      'привычка, либо вознаграждение!')
 
 
 class TimeToCompleteValidator:
@@ -29,7 +30,7 @@ class IsEnjoyableHabitValidator:
         is_current_habit_enjoyable = dict(value).get('is_enjoyable')
         related_habit = dict(value).get('related_habit')
 
-        if not is_current_habit_enjoyable and related_habit and not related_habit.get('is_enjoyable'):
+        if not is_current_habit_enjoyable and related_habit and not related_habit.is_enjoyable:
             raise ValidationError('У полезной привычки в связанных привычках может быть только приятная привычка!')
 
 
@@ -39,5 +40,6 @@ class EnjoyableHabitValidator:
         related_habit = dict(value).get('related_habit')
         reward = dict(value).get('reward')
 
-        if is_current_habit_enjoyable and related_habit or reward:
-            raise ValidationError('У приятной привычки не может быть связанной привычки или вознаграждения!')
+        if is_current_habit_enjoyable:
+            if related_habit or reward:
+                raise ValidationError('У приятной привычки не может быть связанной привычки или вознаграждения!')
