53f9d79c62ff75486bc6dbd587f6953bfccdfb93
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-31 13:42:21 +0400

    Add HabitOrRewardValidator, TimeToCompleteValidator, IsEnjoyableHabitValidator, EnjoyableHabitValidator validators to HabitSerializer, also add UserDetailSerializer to field user

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from habits.models import Habit
+from habits.validators import HabitOrRewardValidator, TimeToCompleteValidator, IsEnjoyableHabitValidator, \
+    EnjoyableHabitValidator
+from users.serializers import UserDetailSerializer
 
 
 class HabitSerializer(serializers.ModelSerializer):
+    user = UserDetailSerializer(read_only=True)
+
     class Meta:
         model = Habit
         fields = '__all__'
+        validators = [HabitOrRewardValidator(), TimeToCompleteValidator(),
+                      IsEnjoyableHabitValidator(), EnjoyableHabitValidator()]
