1b7e3e332fd51a840023a2739513c8686627e231
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-31 17:41:04 +0400

    Add perform_create() in UserCreateAPIView and also add UserDestroyAPIView

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from users.models import User
 from users.permissions import IsCurrentUser
 from users.serializers import UserSerializer
@@ -7,6 +8,12 @@ from users.serializers import UserSerializer
 class UserCreateAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        user.set_password(user.password)
+        user.save()
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
@@ -16,6 +23,12 @@ class UserUpdateAPIView(generics.UpdateAPIView):
 
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsCurrentUser]
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = [IsCurrentUser]
