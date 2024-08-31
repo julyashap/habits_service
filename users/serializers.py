6bcc370f13893572798cf2dc91d0c21f23ba9f54
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-31 16:41:04 +0400

    Update fields atribute in UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -5,7 +5,7 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['pk', 'email', 'password', 'first_name', 'last_name', 'avatar', 'phone', 'country', 'tg_chat_id']
 
 
 class UserDetailSerializer(serializers.ModelSerializer):
