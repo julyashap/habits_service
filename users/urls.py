4db94277c67825bbd5454744643b0b7133d4df11
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-31 17:41:17 +0400

    Add UserDestroyAPIView to urlpatterns

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.apps import UsersConfig
-from users.views import UserCreateAPIView, UserUpdateAPIView, UserRetrieveAPIView
+from users.views import UserCreateAPIView, UserUpdateAPIView, UserRetrieveAPIView, UserDestroyAPIView
 
 app_name = UsersConfig.name
 
@@ -12,4 +12,5 @@ urlpatterns = [
     path('create/', UserCreateAPIView.as_view(), name='user_create'),
     path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
     path('retrieve/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_retrieve'),
+    path('destroy/<int:pk>/', UserDestroyAPIView.as_view(), name='user_destroy'),
 ]
