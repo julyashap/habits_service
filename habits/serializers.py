754b5a0fe6331eb70a1daa4313a4ab624aecf225
Julya Shapaeva <shapaeva03@mail.ru>
2024-08-31 16:42:22 +0400

    Update fields atribute in HabitSerializer and add HabitPublicSerializer

--- habits/serializers.py
+++ habits/serializers.py
@@ -6,10 +6,19 @@ from users.serializers import UserDetailSerializer
 
 
 class HabitSerializer(serializers.ModelSerializer):
-    user = UserDetailSerializer(read_only=True)
+    class Meta:
+        model = Habit
+        fields = ['pk', 'place', 'time', 'action', 'is_enjoyable', 'related_habit', 'periodicity', 'reward',
+                  'time_to_complete', 'is_public', 'created_at', 'user']
+        read_only_fields = ['user', 'created_at']
+        validators = [HabitOrRewardValidator(), TimeToCompleteValidator(),
+                      IsEnjoyableHabitValidator(), EnjoyableHabitValidator()]
+
+
+class HabitPublicSerializer(serializers.ModelSerializer):
+    user = UserDetailSerializer()
 
     class Meta:
         model = Habit
-        fields = '__all__'
-        validators = [HabitOrRewardValidator(), TimeToCompleteValidator(),
-                      IsEnjoyableHabitValidator(), EnjoyableHabitValidator()]
+        fields = ['pk', 'place', 'time', 'action', 'is_enjoyable', 'related_habit', 'periodicity', 'reward',
+                  'time_to_complete', 'created_at', 'user']
