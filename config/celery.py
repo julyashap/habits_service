bf345f76baed96a18255ee724e60a539fba9aa2d
Julya Shapaeva <shapaeva03@mail.ru>
2024-09-02 01:32:51 +0300

    Add app.conf.worker_pool and app.conf.beat_scheduler settings to config.celery

--- config/celery.py
+++ config/celery.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('config')
@@ -9,3 +10,6 @@ app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+app.conf.worker_pool = 'solo'
+app.conf.beat_scheduler = 'django_celery_beat.schedulers:DatabaseScheduler'
