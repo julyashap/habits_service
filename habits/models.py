47d21f1cd4dbf0d41bdb4b33821e21919e0362b4
Julya Shapaeva <shapaeva03@mail.ru>
2024-09-02 00:35:53 +0400

    Replace field periodicity to periodicity_every

--- habits/models.py
+++ habits/models.py
@@ -5,20 +5,13 @@ NULLABLE = {'null': True, 'blank': True}
 
 
 class Habit(models.Model):
-    PERIODICITY_CHOICES = (
-        ('hourly', 'раз в час'),
-        ('daily', 'раз в день'),
-        ('weekly', 'раз в неделю'),
-    )
-
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='пользователь')
     place = models.CharField(max_length=200, verbose_name='место')
     time = models.DateTimeField(verbose_name='время')
     action = models.CharField(max_length=400, verbose_name='действие')
     is_enjoyable = models.BooleanField(verbose_name='признак приятности')
     related_habit = models.ForeignKey('self', on_delete=models.CASCADE, verbose_name='связанная привычка', **NULLABLE)
-    periodicity = models.CharField(max_length=10, choices=PERIODICITY_CHOICES,
-                                   default='daily', verbose_name='периодичность')
+    periodicity_every = models.IntegerField(default=1, verbose_name='каждое кол-во дней')
     reward = models.CharField(max_length=400, verbose_name='вознаграждение', **NULLABLE)
     time_to_complete = models.DurationField(verbose_name='время на выполнение')
     is_public = models.BooleanField(verbose_name='признак публичности', default=False)
