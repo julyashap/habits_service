ca99deb98880c102375e17ecf9ad0a65347bd715
Julya Shapaeva <shapaeva03@mail.ru>
2024-09-02 00:36:10 +0400

    Replace field periodicity to periodicity_every in HabitAdmin

--- habits/admin.py
+++ habits/admin.py
@@ -4,7 +4,7 @@ from habits.models import Habit
 
 @admin.register(Habit)
 class HabitAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'place', 'time', 'action', 'is_enjoyable', 'related_habit', 'periodicity', 'reward',
-                    'time_to_complete', 'is_public',)
+    list_display = ('pk', 'place', 'time', 'action', 'is_enjoyable', 'related_habit', 'periodicity_every',
+                    'reward', 'time_to_complete', 'is_public',)
     list_filter = ('is_enjoyable', 'is_public',)
     search_fields = ('place', 'action',)
